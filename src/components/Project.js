778345107da5bf0917470a29512f4d39cc11aa4a
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2025-06-03 16:17:57 -0400

    wip

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,20 +9,32 @@ function normalizeName(name) {
 
 export function Project({ name, children }) {
   const key = normalizeName(name);
-  const displayName = name.toUpperCase();
+
+  const getDisplayName = () => {
+    switch (key) {
+      case 'ads': return 'ADS';
+      case 'scix': return 'SciX';
+      default: return '';
+    }
+  }
+
+  const displayName = getDisplayName();
 
   const getLogo = () => {
     const commonProps = { alt: `${displayName} logo`, className: styles.projectLogo }
     switch (key) {
       case 'ads': return <Adslogo {...commonProps} />;
-      case 'scix': return <Scixlogo {...commonProps} />
+      case 'scix': return <Scixlogo {...commonProps} style={{ width: '35px' }} />
+      default: return null;
     }
   }
 
   return (
     <div className={styles.projectBlock}>
       <div className={styles.projectHeader}>
-        {getLogo()}
+        <div className={styles.projectLogoWrapper}>
+          {getLogo()}
+        </div>
         <h3 className={styles.projectTitle}>{displayName}</h3>
       </div>
       <div className={styles.projectBody}>
