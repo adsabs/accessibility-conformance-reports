ab1af5774848fd432d8bfbf8f3283a25e3b94b59
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2025-06-03 16:37:45 -0400

    wip

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -5,6 +5,7 @@ import { Support } from '@site/src/components/ConformanceBlock/Support';
 import { NoSupport } from '@site/src/components/ConformanceBlock/NoSupport';
 import { PartialSupport } from '@site/src/components/ConformanceBlock/PartialSupport';
 import { Exception } from '@site/src/components/ConformanceBlock/Exception';
+import { NonApplicable } from '@site/src/components/ConformanceBlock/NonApplicable';
 import { Project } from '@site/src/components/Project';
 
 export default {
@@ -13,6 +14,7 @@ export default {
   Support,
   NoSupport,
   PartialSupport,
+  NonApplicable,
   Exception,
   Project
 };
