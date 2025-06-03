cc1d7a86981d253f9ab6de6e4fac6f74bf5a3f4f
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2025-06-03 16:25:37 -0400

    wip

--- src/components/ConformanceBlock/Exception.js
+++ src/components/ConformanceBlock/Exception.js
@@ -3,7 +3,7 @@ import styles from './ConformanceBlock.module.css';
 
 export const Exception = ({ children }) => (
   <div className={`${styles.base} ${styles.exception}`}>
-    <span className={styles.label}>⚠️ Conformance: EXCEPTION</span>
+    <span className={styles.label}>⚠️ EXCEPTION</span>
     {children}
   </div>
 );
